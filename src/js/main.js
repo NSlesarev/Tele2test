8c567045c322164ff19897980a9a8fed9efb6f78
Nik <64489217+NSFerz@users.noreply.github.com>
2024-03-12 00:08:47 +0300

    справил недочеты по сайту, так же сделал модальное окно при выборе региона, пока еще без JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,28 @@
-// переменные пк
+// переменные пк выбор города
 const changeСity = document.querySelector(".header_city")
 const tableCity = document.querySelector(".city-desctop")
 const headerCity = document.querySelector(".header_city_city")
 
-// переменные мобилки
+// переменные мобилки выбор города
 const changeСityMobile = document.querySelector(".header_city")
 const tableCityMobile = document.querySelector(".city-mobile")
 const headerCityMobile = document.querySelector(".header_city_city")
 
 
-// для пк
+
+const cityConfirmationModal = document.getElementById(".header-planka");
+const confirmCityBtn = document.getElementById(".header-planka__options__yes");
+const changeCityBtn = document.getElementById(".header-planka__options__change");
+const currentCitySpan = document.getElementById("currentCity");
+
+
+
+
+  
+
+
+
+// для пк выбор города
 changeСity.addEventListener("click",() => {
     tableCity.classList.toggle("visible")
 })
@@ -21,7 +34,7 @@ headerCity.innerHTML = event.target.innerHTML;
 };
 
 
-// для мобилки
+// для мобилки выбор города
 changeСityMobile.addEventListener("click",() => {
     tableCityMobile.classList.toggle("visible")
 })
